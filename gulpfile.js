dfa3a7dd4f1765c08a7a59271304de08ffeb0708
James Monk <james@jamesmonk.me>
2021-11-10 18:06:22 +0100

    Gulpfile and npm packages update, bugfixing and amends since site transfered to AWS server. Weird crossorigin issue with fonts even though they sit on same server - updated a new htaccess to address and added to repo, removed crossorigin call from font preloads, also.

--- gulpfile.js
+++ gulpfile.js
@@ -1,250 +1,103 @@
-'use strict';
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
 
-//
-// Packages
-//
+import gulp from 'gulp';
+import del from 'del';
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+const sass = gulpSass( dartSass );
+import cleanCSS from 'gulp-clean-css';
+import concat from 'gulp-concat';
+import uglify from 'gulp-uglify';
+import svgsprite from 'gulp-svg-sprite';
+import imagemin, {gifsicle, mozjpeg, optipng, svgo} from 'gulp-imagemin';
+import hb from 'gulp-hb';
+import ext from 'gulp-ext-replace'
 
-const gulp = require('gulp');
-const del = require('del');
-const svgsprite = require('gulp-svg-sprite');
-const imagemin = require('gulp-imagemin');
-const jshint = require('gulp-jshint');
-const stylish = require('jshint-stylish');
-const cleanCSS = require('gulp-clean-css');
-const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
-const sasslint = require('gulp-sass-lint');
-const ext = require('gulp-ext-replace');
-const uglify = require('gulp-uglify');
-const concat = require('gulp-concat');
-const hb = require('gulp-hb');
-const log = require('fancy-log');
+export const clean = () => del([ 'dist/' ]);
 
-
-//
-// Processes
-//
-
-// Clean
-function clean() {
-  return del('./dist');
-}
-
-//
-// JS
-//
-
-function jslint() {
-  return gulp
-    .src([
-      './src/js/*/js',
-      './gulpfile.js'
-    ], { allowEmpty: true })
-    .pipe(jshint())
-    .pipe(jshint.reporter(stylish));
-}
-
-function jslazyloadmodules() {
-  return gulp
-    .src([
-      './node_modules/loadjs/dist/loadjs.min.js'
-    ], { allowEmpty: true })
-    .pipe(ext('.hbs'))
-    .pipe(gulp.dest('./src/html/partials/global/'));
-}
-
-function jscritical() {
-  return gulp
-    .src('./src/js/critical.js', { allowEmpty: true })
-    .pipe(uglify({
-      mangle: true,
-      compress: {
-        sequences: true,
-        dead_code: true,
-        conditionals: true,
-        booleans: true,
-        unused: true,
-        if_return: true,
-        join_vars: true,
-        drop_console: false
-      }}
-    ))
-    .pipe(concat('js.js'))
-    .pipe(ext('.hbs'))
-    .pipe(gulp.dest('./src/html/partials/global/'));
-}
-
-function jsnoncritical() {
-  return gulp
-    .src([
-      './node_modules/bootstrap/dist/js/bootstrap.bundle.js',
-      './src/js/main.js'
-    ], { allowEmpty: true })
-    .pipe(uglify({
-      mangle: true,
-      compress: {
-        sequences: true,
-        dead_code: true,
-        conditionals: true,
-        booleans: true,
-        unused: true,
-        if_return: true,
-        join_vars: true,
-        drop_console: false
-      }}
-    ))
+export function scripts() {
+  return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.bundle.js', 'src/js/main.js'], { sourcemaps: true })
+    .pipe(uglify())
     .pipe(concat('main.js'))
     .pipe(gulp.dest('./dist/wp-content/themes/synthetic/js/'));
 }
 
-
-//
-// CSS
-//
-
-function csslint() {
-  return gulp
-    .src('./src/scss/*.scss', { allowEmpty: true })
-    .pipe(sasslint({'configFile': './.sass-lint.yml'}))
-    .pipe(sasslint.format())
-    .pipe(sasslint.failOnError());
-}
-
-function cssnoncritical() {
-  return gulp
-    .src('./src/scss/style.scss', { allowEmpty: true })
+export function styles() {
+  return gulp.src('src/scss/style.scss')
     .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(autoprefixer())
     .pipe(cleanCSS())
     .pipe(gulp.dest('./dist/wp-content/themes/synthetic/'));
 }
-
 
 //
 // Assets
 //
 
-function sprite() {
-  return gulp
-    .src('./src/sprite/**/**/*.svg', { allowEmpty: true })
-    .pipe(
-      svgsprite({
-        shape: {
-          spacing: {
-            padding: 5
-          }
-        },
-        mode: {
-          symbol: true
-        },
-        svg: {
-          xmlDeclaration: false,
-          doctypeDeclaration: false,
-          namespaceIDs: false,
-          namespaceClassnames: false
-        }
-      })
-    )
+export function sprite() {
+  return gulp.src('src/sprite/**/**/*.svg')
+    .pipe(svgsprite({
+      shape: { spacing: { padding: 5 } },
+      mode: { symbol: true },
+      svg: { xmlDeclaration: false, doctypeDeclaration: false, namespaceIDs: false, namespaceClassnames: false }
+    }))
     .pipe(concat('sprite.hbs'))
     .pipe(gulp.dest('./src/html/partials/global/'));
 }
 
-function images() {
-  return gulp
-    .src('./src/img/**/**/*', { allowEmpty: true })
-    .pipe(
-      imagemin([
-        imagemin.gifsicle({ interlaced: true }),
-        imagemin.mozjpeg({ quality: 80, progressive: true }),
-        imagemin.optipng({ optimizationLevel: 5 }),
-        imagemin.svgo({
-          plugins: [
-            {
-              removeViewBox: false,
-              collapseGroups: true
-            }
-          ]
-        })
-      ])
-    )
+export function images() {
+  return gulp.src('src/img/**/**/*')
+    .pipe(imagemin([
+      gifsicle({interlaced: true}),
+      mozjpeg({quality: 75, progressive: true}),
+      optipng({optimizationLevel: 5}),
+      svgo({
+        plugins: [
+          {
+            name: 'removeViewBox',
+            active: true
+          },
+          {
+            name: 'cleanupIDs',
+            active: true
+          },
+          {
+            name: 'collapseGroups',
+            active: true
+          }
+        ]
+      })
+    ]))
     .pipe(gulp.dest('./dist/wp-content/themes/synthetic/img/'));
 }
 
-function fonts() {
-  return gulp
-    .src('./src/fonts/*', { allowEmpty: true })
+export function fonts() {
+  return gulp.src('./src/fonts/*', { allowEmpty: true })
     .pipe(gulp.dest('./dist/wp-content/themes/synthetic/fonts/'));
 }
 
+export function htaccess() {
+  return gulp.src('./src/htaccess/.htaccess', { allowEmpty: true })
+    .pipe(gulp.dest('./dist/'));
+}
 
-//
-// HTML
-//
-
-// HTML
-function html() {
-  return gulp
-    .src(['./src/html/**/*.hbs', '!./src/html/partials/**/*.hbs'])
-    .pipe(hb()
-      .partials('./src/html/partials/**/*.hbs')
-    )
+export function html() {
+  return gulp.src(['src/html/**/**/*.hbs', '!src/html/partials/**/*.hbs'])
+    .pipe(hb().partials('src/html/partials/**/*.hbs'))
     .pipe(ext('.php'))
     .pipe(gulp.dest('./dist/wp-content/themes/synthetic/'));
 }
 
-
-//
-// Watch and build scripts
-//
-
-const watch =
-  gulp.series(
-    clean,
-    gulp.parallel(
-      sprite,
-      images,
-      jslint,
-      csslint
-    ),
-    fonts,
-    html,
-    gulp.parallel(
-      cssnoncritical,
-      jscritical,
-      jsnoncritical
-    ),
-    jslazyloadmodules,
-    html,
-    watchFiles
-  );
-
-const csswatch =
-  gulp.series(
-    csslint,
-    cssnoncritical,
-    html
-  );
-
-const jswatch =
-  gulp.series(
-    jslint,
-    jscritical,
-    jsnoncritical,
-    html
-  );
-
 function watchFiles() {
-  gulp.watch('./src/scss/**/*.scss', csswatch);
-  gulp.watch('./src/js/**/*.js', jswatch);
-  gulp.watch('./src/sprite/**/*.svg', sprite);
-  gulp.watch('./src/html/**/*.hbs', csswatch, html);
-  gulp.watch('./src/img/**/*', images);
-  gulp.watch('./src/fonts/*', fonts);
+  gulp.watch('src/js/**/*.js', scripts);
+  gulp.watch('src/sprite/**/*.svg', sprite);
+  gulp.watch('src/scss/**/*', styles);
+  gulp.watch('src/html/**/**/*.hbs', html);
+  gulp.watch('src/htaccess/*', htaccess);
+  gulp.watch('src/img/**/*', images);
+  gulp.watch('src/fonts/**/*', fonts);
 }
 
+const build = gulp.series(clean, gulp.parallel(htaccess, sprite, images, styles, fonts, scripts), html, watchFiles);
 
-//
-// Export tasks
-//
-
-exports.default = watch;
+export default build;
