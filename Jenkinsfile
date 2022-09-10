2becb2fca9e3b13170c546e1cb9b3a31cc5c00eb
James Monk <james@jamesmonk.me>
2022-09-10 20:03:37 +0100

    Jenkinsfile update for SFTP push to Lightsail deployment

--- Jenkinsfile
+++ Jenkinsfile
@@ -2,9 +2,58 @@ pipeline {
   agent any
 
   stages {
-    stage('Do nothing') {
+    stage('Npm install') {
       steps {
-        sh '/bin/true'
+        sh 'npm i'
+      }
+    }
+    stage('Gulp build') {
+      steps {
+        sh 'gulp build'
+      }
+    }
+    stage('Deploy') {
+      steps {
+        sshPublisher(
+          publishers: [
+            sshPublisherDesc(
+              configName: 'Lightsail AWS (Synthetic)',
+              transfers: [
+                sshTransfer(
+                  cleanRemote: false,
+                  excludes: '',
+                  execCommand: 'rm -rf *',
+                  execTimeout: 120000,
+                  flatten: false,
+                  makeEmptyDirs: false,
+                  noDefaultExcludes: false,
+                  patternSeparator: '[, ]+',
+                  remoteDirectory: '',
+                  remoteDirectorySDF: false,
+                  removePrefix: '',
+                  sourceFiles: ''
+                ),
+                sshTransfer(
+                  cleanRemote: false,
+                  excludes: '',
+                  execCommand: '',
+                  execTimeout: 120000,
+                  flatten: false,
+                  makeEmptyDirs: false,
+                  noDefaultExcludes: false,
+                  patternSeparator: '[, ]+',
+                  remoteDirectory: '',
+                  remoteDirectorySDF: false,
+                  removePrefix: 'dist',
+                  sourceFiles: 'dist/**/*'
+                )
+              ],
+              usePromotionTimestamp: false,
+              useWorkspaceInPromotion: false,
+              verbose: true
+            )
+          ]
+        )
       }
     }
   }
--- gulpfile.js
+++ gulpfile.js
@@ -20,14 +20,14 @@ export function scripts() {
   return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.bundle.js', 'src/js/main.js'], { sourcemaps: true })
     .pipe(uglify())
     .pipe(concat('main.js'))
-    .pipe(gulp.dest('./dist/wp-content/themes/synthetic/js/'));
+    .pipe(gulp.dest('./dist/js/'));
 }
 
 export function styles() {
   return gulp.src('src/scss/style.scss')
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(cleanCSS())
-    .pipe(gulp.dest('./dist/wp-content/themes/synthetic/'));
+    .pipe(gulp.dest('./dist/'));
 }
 
 //
@@ -68,24 +68,19 @@ export function images() {
         ]
       })
     ]))
-    .pipe(gulp.dest('./dist/wp-content/themes/synthetic/img/'));
+    .pipe(gulp.dest('./dist/img/'));
 }
 
 export function fonts() {
   return gulp.src('./src/fonts/*', { allowEmpty: true })
-    .pipe(gulp.dest('./dist/wp-content/themes/synthetic/fonts/'));
-}
-
-export function htaccess() {
-  return gulp.src('./src/htaccess/.htaccess', { allowEmpty: true })
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest('./dist/fonts/'));
 }
 
 export function html() {
   return gulp.src(['src/html/**/**/*.hbs', '!src/html/partials/**/*.hbs'])
     .pipe(hb().partials('src/html/partials/**/*.hbs'))
     .pipe(ext('.php'))
-    .pipe(gulp.dest('./dist/wp-content/themes/synthetic/'));
+    .pipe(gulp.dest('./dist/'));
 }
 
 function watchFiles() {
@@ -93,11 +88,11 @@ function watchFiles() {
   gulp.watch('src/sprite/**/*.svg', sprite);
   gulp.watch('src/scss/**/*', styles);
   gulp.watch('src/html/**/**/*.hbs', html);
-  gulp.watch('src/htaccess/*', htaccess);
   gulp.watch('src/img/**/*', images);
   gulp.watch('src/fonts/**/*', fonts);
 }
 
-const build = gulp.series(clean, gulp.parallel(htaccess, sprite, images, styles, fonts, scripts), html, watchFiles);
+export const build = gulp.series(clean, gulp.parallel(sprite, images, styles, fonts, scripts), html);
+const watch = gulp.series(build, watchFiles);
 
-export default build;
+export default watch;
